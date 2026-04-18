86347b0a5b9e232176d0e9d3c6270312beae932a
Lasse Hansen <lassehhansen02@gmail.com>
2026-04-18 20:53:49 +0200

    Made the website design look better for phones, and added a pop up page when you press see more on the project side

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const greeting = document.getElementById("greeting");
 const hour = new Date().getHours();
 
-if (hour < 12) {
+if (greeting)
+{
+    const hour = new Date().getHours();
+    if (hour < 12) {
     greeting.innerText = "Godmorgen og velkommen til 👋";
 }   
     else if (hour < 18) {
@@ -11,3 +14,39 @@ if (hour < 12) {
     else {
         greeting.innerText = "Godaften og velkommen til 👋";
     }
+}
+
+
+
+
+const modal = document.getElementById("projectModal");
+const closeBtn = document.getElementById(".close-btn");
+
+function openModal(title, description) {
+    document.getElementById("projectModal");
+    document.getElementById("modalTitle").innerText = title;
+    document.getElementById("modalDescription").innerText = description;
+    
+    if (modal && modalTitle && modalDescription)
+    {
+        modalTitle.innerText = title;
+        modalDescription.innerText = description;
+        modal.style.display = "block";
+    }
+}
+
+function closeModal() {
+    const modal = document.getElementById("projectModal");
+    if (modal)
+    {
+        modal.style.display = "none";
+    }
+    
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById("projectModal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
